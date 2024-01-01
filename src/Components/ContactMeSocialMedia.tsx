d25242224461a70ece80f7ecfda3a2b03899d171
fasalmohammadh <fazalmohammed9367@gmail.com>
2024-01-01 22:21:55 +0530

    fix: fixed ts type errors that caused build failure

--- src/Components/ContactMeSocialMedia.tsx
+++ src/Components/ContactMeSocialMedia.tsx
@@ -1,66 +1,66 @@
-import React from 'react';
+import React from "react";
 
-import { Stack, GlobalStyles, BoxProps, IconButton } from '@mui/material';
+import { Stack, GlobalStyles, StackProps, IconButton } from "@mui/material";
 
-import FacebookIcon from '../assets/SocialMediaLogos/FBLogo.webp';
-import LinkedInIcon from '../assets/SocialMediaLogos/linkedinLogo.webp';
-import InstagramIcon from '../assets/SocialMediaLogos/instagramLogo.webp';
-import WhatsappIcon from '../assets/SocialMediaLogos/whatsappLogo.webp';
-import GitHubIcon from '../assets/SocialMediaLogos/github.png';
+import FacebookIcon from "../assets/SocialMediaLogos/FBLogo.webp";
+import LinkedInIcon from "../assets/SocialMediaLogos/linkedinLogo.webp";
+import InstagramIcon from "../assets/SocialMediaLogos/instagramLogo.webp";
+import WhatsappIcon from "../assets/SocialMediaLogos/whatsappLogo.webp";
+import GitHubIcon from "../assets/SocialMediaLogos/github.png";
 
-const ContactMeSocialMedia = (props: BoxProps) => {
+const ContactMeSocialMedia = (props: StackProps) => {
   return (
-    <Stack flexDirection='row' {...props}>
-      <GlobalStyles styles={{ img: { minWidth: '0', width: '100%' } }} />
+    <Stack flexDirection="row" {...props}>
+      <GlobalStyles styles={{ img: { minWidth: "0", width: "100%" } }} />
 
       <IconButton
-        sx={{ flex: '1 1 0' }}
-        href='https://www.linkedin.com/in/fazal-mohamed-99211b235'
-        target='_blank'
-        rel='noopener noreferrer'
-        aria-label='Linked In'
+        sx={{ flex: "1 1 0" }}
+        href="https://www.linkedin.com/in/fazal-mohamed-99211b235"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Linked In"
       >
-        <img src={LinkedInIcon} alt='Linked In Icon' />
+        <img src={LinkedInIcon} alt="Linked In Icon" />
       </IconButton>
 
       <IconButton
-        sx={{ flex: '1 1 0' }}
-        href='https://www.instagram.com/fasalmohammadh/'
-        target='_blank'
-        rel='noopener noreferrer'
-        aria-label='Instagram'
+        sx={{ flex: "1 1 0" }}
+        href="https://www.instagram.com/fasalmohammadh/"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Instagram"
       >
-        <img src={InstagramIcon} alt='Instagram Icon' />
+        <img src={InstagramIcon} alt="Instagram Icon" />
       </IconButton>
 
       <IconButton
-        sx={{ flex: '1 1 0' }}
-        href='https://www.facebook.com/fazal.mohamed.125323'
-        target='_blank'
-        rel='noopener noreferrer'
-        aria-label='Facebook'
+        sx={{ flex: "1 1 0" }}
+        href="https://www.facebook.com/fazal.mohamed.125323"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Facebook"
       >
-        <img src={FacebookIcon} alt='Facebook Icon' />
+        <img src={FacebookIcon} alt="Facebook Icon" />
       </IconButton>
 
       <IconButton
-        sx={{ flex: '1 1 0' }}
-        href='https://wa.me/94778178500'
-        target='_blank'
-        rel='noopener noreferrer'
-        aria-label='Whatsapp'
+        sx={{ flex: "1 1 0" }}
+        href="https://wa.me/94778178500"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Whatsapp"
       >
-        <img src={WhatsappIcon} alt='Whatsapp Icon' />
+        <img src={WhatsappIcon} alt="Whatsapp Icon" />
       </IconButton>
 
       <IconButton
-        sx={{ flex: '1 1 0' }}
-        href='https://github.com/FasalMohammadh'
-        target='_blank'
-        rel='noopener noreferrer'
-        aria-label='Github'
+        sx={{ flex: "1 1 0" }}
+        href="https://github.com/FasalMohammadh"
+        target="_blank"
+        rel="noopener noreferrer"
+        aria-label="Github"
       >
-        <img src={GitHubIcon} alt='Github Icon' />
+        <img src={GitHubIcon} alt="Github Icon" />
       </IconButton>
     </Stack>
   );
